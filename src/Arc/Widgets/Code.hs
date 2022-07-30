b09e22149e592dccf39e9a5d547c11a8c4029bdc
ners <ners@gmx.ch>
2022-07-30 18:40:52 +0200

    Add light and dark themes to syntax highlighting

--- src/Arc/Widgets/Code.hs
+++ src/Arc/Widgets/Code.hs
@@ -3,6 +3,7 @@ module Arc.Widgets.Code where
 import Arc.Util
 import Control.Monad (forM_)
 import Data.Default (Default)
+import Data.String (IsString)
 import Data.Text (Text)
 import qualified Data.Text as Text
 import qualified GHC.SyntaxHighlighter as GS
@@ -18,7 +19,7 @@ instance Default CodeBlock where
     def = CodeBlock Nothing ""
 
 codeBlock :: DomBuilder t m => CodeBlock -> m ()
-codeBlock CodeBlock{..} = el "pre" $
+codeBlock CodeBlock{..} = elClass "pre" "code" $
     el "code" $ do
         case codeBlockLanguage of
             Just "Haskell" -> highlightHaskell codeBlockContent
@@ -44,56 +45,59 @@ highlightHaskell hs = case GS.tokenizeHaskell hs of
     Just toks -> mapM_ token toks
     Nothing -> text hs
 
+tokenTag :: IsString s => s
+tokenTag = "span"
+
 token :: (IsToken s, DomBuilder t m) => (s, Text) -> m ()
-token (tokenType, txt) = elClass "span" (tokenClass tokenType) $ text txt
+token (tokenType, txt) = elClass tokenTag (tokenClass tokenType) $ text txt
 
 class IsToken t where
     tokenClass :: t -> Text
 
-instance IsToken GS.Token where
-    tokenClass GS.CharTok = "ch"
-    tokenClass GS.CommentTok = "co"
-    tokenClass GS.ConstructorTok = "cr"
-    tokenClass GS.IntegerTok = "it"
-    tokenClass GS.KeywordTok = "kw"
-    tokenClass GS.OperatorTok = "op"
-    tokenClass GS.OtherTok = "ot"
-    tokenClass GS.PragmaTok = "pr"
-    tokenClass GS.RationalTok = "ra"
-    tokenClass GS.SpaceTok = ""
-    tokenClass GS.StringTok = "st"
-    tokenClass GS.SymbolTok = "sy"
-    tokenClass GS.VariableTok = "va"
-
 instance IsToken S.TokenType where
-    tokenClass S.KeywordTok = "kw"
+    tokenClass S.AlertTok = "al"
+    tokenClass S.AnnotationTok = "an"
+    tokenClass S.AttributeTok = "at"
+    tokenClass S.BaseNTok = "bn"
+    tokenClass S.BuiltInTok = "bu"
+    tokenClass S.CharTok = "ch"
+    tokenClass S.CommentTok = "co"
+    tokenClass S.CommentVarTok = "cv"
+    tokenClass S.ConstantTok = "cn"
+    tokenClass S.ControlFlowTok = "cf"
     tokenClass S.DataTypeTok = "dt"
     tokenClass S.DecValTok = "dv"
-    tokenClass S.BaseNTok = "bn"
-    tokenClass S.FloatTok = "fl"
-    tokenClass S.CharTok = "ch"
-    tokenClass S.StringTok = "st"
-    tokenClass S.CommentTok = "co"
-    tokenClass S.OtherTok = "ot"
-    tokenClass S.AlertTok = "al"
-    tokenClass S.FunctionTok = "fu"
-    tokenClass S.RegionMarkerTok = "re"
-    tokenClass S.ErrorTok = "er"
-    tokenClass S.ConstantTok = "cn"
-    tokenClass S.SpecialCharTok = "sc"
-    tokenClass S.VerbatimStringTok = "vs"
-    tokenClass S.SpecialStringTok = "ss"
-    tokenClass S.ImportTok = "im"
     tokenClass S.DocumentationTok = "do"
-    tokenClass S.AnnotationTok = "an"
-    tokenClass S.CommentVarTok = "cv"
-    tokenClass S.VariableTok = "va"
-    tokenClass S.ControlFlowTok = "cf"
-    tokenClass S.OperatorTok = "op"
-    tokenClass S.BuiltInTok = "bu"
+    tokenClass S.ErrorTok = "er"
     tokenClass S.ExtensionTok = "ex"
-    tokenClass S.PreprocessorTok = "pp"
-    tokenClass S.AttributeTok = "at"
+    tokenClass S.FloatTok = "fl"
+    tokenClass S.FunctionTok = "fu"
+    tokenClass S.ImportTok = "im"
     tokenClass S.InformationTok = "in"
+    tokenClass S.KeywordTok = "kw"
+    tokenClass S.OperatorTok = "op"
+    tokenClass S.OtherTok = "ot"
+    tokenClass S.PreprocessorTok = "pp"
+    tokenClass S.RegionMarkerTok = "re"
+    tokenClass S.SpecialCharTok = "sc"
+    tokenClass S.SpecialStringTok = "ss"
+    tokenClass S.StringTok = "st"
+    tokenClass S.VariableTok = "va"
+    tokenClass S.VerbatimStringTok = "vs"
     tokenClass S.WarningTok = "wa"
     tokenClass _ = ""
+
+instance IsToken GS.Token where
+    tokenClass GS.CharTok = tokenClass S.CharTok
+    tokenClass GS.CommentTok = tokenClass S.CommentTok
+    tokenClass GS.ConstructorTok = tokenClass S.FunctionTok
+    tokenClass GS.IntegerTok = tokenClass S.ConstantTok
+    tokenClass GS.KeywordTok = tokenClass S.KeywordTok
+    tokenClass GS.OperatorTok = tokenClass S.OperatorTok
+    tokenClass GS.OtherTok = tokenClass S.OtherTok
+    tokenClass GS.PragmaTok = tokenClass S.PreprocessorTok
+    tokenClass GS.RationalTok = tokenClass S.FloatTok
+    tokenClass GS.SpaceTok = ""
+    tokenClass GS.StringTok = tokenClass S.StringTok
+    tokenClass GS.SymbolTok = tokenClass S.OperatorTok
+    tokenClass GS.VariableTok = tokenClass S.VariableTok
